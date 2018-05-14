2591e262ca858e40899885a9b594567d368dbc61
dario <dario@igenius.net>
2018-05-14 12:13:13 +0200

    PGN tags loading first test

--- src/loader.py
+++ src/loader.py
@@ -1,2 +1,11 @@
+import chess.pgn as pgn_reader
+
+
 class Loader:
-    pass
+    def __init__(self):
+        pass
+
+    def load_single_file(self, path):
+        pgn = open(path)
+        game = pgn_reader.read_game(pgn)
+        return game
--- test/test_loader.py
+++ test/test_loader.py
@@ -1,6 +1,11 @@
 from src.loader import Loader
+import os.path
+
+curr_path = os.path.abspath(os.path.dirname(__file__))
+test_pgn_path = os.path.join(curr_path, 'pgn_files/game_000.pgn')
 
 
 def test_loader():
     loader = Loader()
-    assert loader
+    game = loader.load_single_file(test_pgn_path)
+    assert game.headers['Result'] == "1-0"
