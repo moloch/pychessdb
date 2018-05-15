85ca61abb968016dd2864d0172395e16401be5d4
dario <dario@igenius.net>
2018-05-15 10:58:50 +0200

    Moved Game class inside models module

--- src/loader.py
+++ src/loader.py
@@ -1,60 +1,6 @@
 import chess.pgn
-from peewee import *
-from decouple import config
+from src.models import Game
 from src.pgn_tags import pgn_to_columns
-
-db = PostgresqlDatabase(
-    config('DB_SCHEMA'),
-    user=config('DB_USER'),
-    password=config('DB_PASSWD'),
-    host=config('DB_HOST'),
-    port=config('DB_PORT')
-)
-
-
-class Game(Model):
-    id = BigAutoField(primary_key=True, unique=True)
-    event = CharField()
-    site = CharField()
-    date = CharField()
-    round = CharField()
-    white = CharField()
-    black = CharField()
-    result = CharField()
-    white_title = CharField()
-    black_title = CharField()
-    white_elo = CharField()
-    black_elo = CharField()
-    white_uscf = CharField()
-    black_uscf = CharField()
-    white_na = CharField()
-    black_na = CharField()
-    white_type = CharField()
-    black_type = CharField()
-    event_date = CharField()
-    event_sponsor = CharField()
-    section = CharField()
-    stage = CharField()
-    board = CharField()
-    opening = CharField()
-    variation = CharField()
-    sub_variation = CharField()
-    eco = CharField()
-    nic = CharField()
-    time = CharField()
-    utc_time = CharField()
-    utc_date = CharField()
-    time_control = CharField()
-    set_up = CharField()
-    fen = CharField()
-    termination = CharField()
-    annotator = CharField()
-    mode = CharField()
-    ply_count = CharField()
-    pgn = TextField()
-
-    class Meta:
-        database = db
 
 
 class Loader:
---
+++ src/models.py
@@ -0,0 +1,55 @@
+from decouple import config
+from peewee import *
+
+db = PostgresqlDatabase(
+    config('DB_SCHEMA'),
+    user=config('DB_USER'),
+    password=config('DB_PASSWD'),
+    host=config('DB_HOST'),
+    port=config('DB_PORT')
+)
+
+
+class Game(Model):
+    id = BigAutoField(primary_key=True, unique=True)
+    event = CharField()
+    site = CharField()
+    date = CharField()
+    round = CharField()
+    white = CharField()
+    black = CharField()
+    result = CharField()
+    white_title = CharField()
+    black_title = CharField()
+    white_elo = CharField()
+    black_elo = CharField()
+    white_uscf = CharField()
+    black_uscf = CharField()
+    white_na = CharField()
+    black_na = CharField()
+    white_type = CharField()
+    black_type = CharField()
+    event_date = CharField()
+    event_sponsor = CharField()
+    section = CharField()
+    stage = CharField()
+    board = CharField()
+    opening = CharField()
+    variation = CharField()
+    sub_variation = CharField()
+    eco = CharField()
+    nic = CharField()
+    time = CharField()
+    utc_time = CharField()
+    utc_date = CharField()
+    time_control = CharField()
+    set_up = CharField()
+    fen = CharField()
+    termination = CharField()
+    annotator = CharField()
+    mode = CharField()
+    ply_count = CharField()
+    pgn = TextField()
+
+    class Meta:
+        database = db
