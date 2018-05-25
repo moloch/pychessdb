ecad5db33c2f6b7f7fff0e3653ad1718cf9e67be
dario <dario@igenius.net>
2018-05-25 16:01:19 +0200

    HtmlExporter now inherits from StringExporter

--- src/exporter.py
+++ src/exporter.py
@@ -1,91 +1,9 @@
 import itertools
 import chess
-from chess.pgn import BaseVisitor
+from chess.pgn import StringExporter
 
 
-class HtmlExporter(BaseVisitor):
-    """
-    Allows exporting a game as an HTML
-
-    >>> import chess.pgn
-    >>>
-    >>> game = chess.pgn.Game()
-    >>>
-    >>> exporter = chess.pgn.StringExporter(headers=True, variations=True, comments=True)
-    >>> pgn_string = game.accept(exporter)
-
-    Only *columns* characters are written per line. If *columns* is ``None``
-    then the entire movetext will be on a single line. This does not affect
-    header tags and comments.
-
-    There will be no newline characters at the end of the string.
-    """
-
-    def __init__(self, columns=80, headers=True, comments=True, variations=True):
-        self.columns = columns
-        self.headers = headers
-        self.comments = comments
-        self.variations = variations
-
-        self.found_headers = False
-
-        self.force_movenumber = True
-
-        self.lines = []
-        self.current_line = ""
-        self.variation_depth = 0
-
-    def flush_current_line(self):
-        if self.current_line:
-            self.lines.append(self.current_line.rstrip())
-        self.current_line = ""
-
-    def write_token(self, token):
-        if self.columns is not None and self.columns - len(self.current_line) < len(token):
-            self.flush_current_line()
-        self.current_line += token
-
-    def write_line(self, line=""):
-        self.flush_current_line()
-        self.lines.append(line.rstrip())
-
-    def end_game(self):
-        self.write_line()
-
-    def begin_headers(self):
-        self.found_headers = False
-
-    def visit_header(self, tagname, tagvalue):
-        if self.headers:
-            self.found_headers = True
-            self.write_line("[{0} \"{1}\"]".format(tagname, tagvalue))
-
-    def end_headers(self):
-        if self.found_headers:
-            self.write_line()
-
-    def begin_variation(self):
-        self.variation_depth += 1
-
-        if self.variations:
-            self.write_token("( ")
-            self.force_movenumber = True
-
-    def end_variation(self):
-        self.variation_depth -= 1
-
-        if self.variations:
-            self.write_token(") ")
-            self.force_movenumber = True
-
-    def visit_comment(self, comment):
-        if self.comments and (self.variations or not self.variation_depth):
-            self.write_token("{ " + comment.replace("}", "").strip() + " } ")
-            self.force_movenumber = True
-
-    def visit_nag(self, nag):
-        if self.comments and (self.variations or not self.variation_depth):
-            self.write_token("$" + str(nag) + " ")
+class HtmlExporter(StringExporter):
 
     def visit_move(self, board, move):
         if self.variations or not self.variation_depth:
@@ -99,15 +17,3 @@ class HtmlExporter(BaseVisitor):
             self.write_token(board.san(move) + "</a> ")
 
             self.force_movenumber = False
-
-    def visit_result(self, result):
-        self.write_token(result + " ")
-
-    def result(self):
-        if self.current_line:
-            return "\n".join(itertools.chain(self.lines, [self.current_line.rstrip()])).rstrip()
-        else:
-            return "\n".join(self.lines).rstrip()
-
-    def __str__(self):
-        return self.result()
